57718c871ffca03b4aab7755254ca0c12d66c5d1
mehul <you@example.com>
2023-06-30 12:49:34 +0530

    new files

---
+++ script.js
@@ -0,0 +1,12 @@
+var typed = new Typed("#element", {
+  strings: [
+    "React Developer",
+    "Android Developer",
+    "Graphic Designer",
+    "Video Editor",
+  ],
+  typeSpeed: 50,
+});
+document.addEventListener("contextmenu", function (e) {
+  e.preventDefault();
+});
